47d814810420b3d77372a81959b21f7d283cfeff
Halex Viotto Gomes <halexx002@gmail.com>
2020-08-13 18:06:22 -0400

    Finished project

---
+++ ssys_employee_manager/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from core.views import EmployeeViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'employees', EmployeeViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
+]
